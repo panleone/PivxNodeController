afd2de0b6e283306b9a8ff2800d43320e4ac81d2
Shangaman <user.email alessandrorezzi2000@gmail.com>
2022-11-04 23:38:09 +0100

    testing if getmnstatus works

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ fetch('http://127.0.0.1:51473/', {
 	}
 })();
 
-async function getgoogle(){
+async function test_balance(){
   try{
     let output= await fetch('http://127.0.0.1:51473/', {
     method: 'POST',
@@ -49,15 +49,41 @@ async function getgoogle(){
   }
 }
 
+async function GetMnStatus(txid){
+  try{
+    let output= await fetch('http://127.0.0.1:51473/', {
+    method: 'POST',
+    headers: {
+        'content-type': 'text/plain;',
+        'Authorization': 'Basic ' + encodeBase64('masternode_test:p')
+    },
+    body: '{"jsonrpc": "1.0", "id":"curltest", "method": "listmasternodes", "params": ['+txid+'] }'});
+    return output.text()
+  }catch(error){
+    if (error.name === 'AbortError') {
+			return "request was aborted'";
+		}else{
+      return "non u sac";
+    }
+  }
+}
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
  app.get('/getbalance', async (req, res) => {
-  res.send(await getgoogle());
+  res.send(await test_balance());
   })
+  //ce9efce67bf55fed12a162ab3c02e5b58e8f69ceac51524ae067ab06ad558a7f
+
+
+  app.get('/mnstatus', async function(req, res) {
+    //res.send('txid ' + req.query.txid);    
+    res.send(await GetMnStatus(req.query.txid))
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

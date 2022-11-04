ea7910ba54ef31a1042c070c51b478f3212097f2
Shangaman <user.email alessandrorezzi2000@gmail.com>
2022-11-04 22:49:20 +0100

    test commit to see if I can open the port

--- app.js
+++ app.js
@@ -4,29 +4,34 @@ import fetch from "node-fetch";
 const app = express()
 const port = 3000
 
+export const encodeBase64 = (data) => {
+    return Buffer.from(data).toString('base64');
+}
+
+/*
 fetch('http://127.0.0.1:51473/', {
     method: 'POST',
     headers: {
         'content-type': 'text/plain;',
-        'Authorization': 'Basic ' + btoa('masternode_test:p')
+        'Authorization': 'Basic ' + encodeBase64btoa('masternode_test:p')
     },
     body: '{"jsonrpc": "1.0", "id":"curltest", "method": "getbalance", "params": [] }'
 }).then(res => res.text())
 .then(text => console.log(text));
-
-/*
-fetch('https://google.com')
-    .then(res => res.text())
-    .then(text => console.log(text));
 */
+async function getgoogle(){
+  let output= await fetch('https://google.com')
+  return await output.text()
+}
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/porcodio', (req, res) => {
-    res.send('dio porco World!')
+ app.get('/getbalance', async (req, res) => {
+  res.send(await getgoogle());
   })
 
 app.listen(port, () => {
